0d0a5bd170d2df74ef9df0259908945ac296371a
Fuyao Zhao <zfy0701@gmail.com>
2023-11-06 09:37:54 +0800

    migrate sentio tracers
    fix tracemany error format
    
    migrate override and gas changes (#53)
    
    fix race condition in p2p discover tab
    
    add more information to root trace
    
    correct handle call with value
    
    set transfer value to zero if can't transfer
    
    migrate memory compression (#7)
    
    correctly handle tracecallmany didn't find block
    
    correctly handle if transaction index out of bound
    
    fix(rpc): eth_blockNumber returns unsynced block number
    
    adjust after api change
    
    Add Mapping keys to post account (#9)
    
    fix when tracer failed before start and zero gas (#11)
    
    more fix on tracecall many format error
    
    fix txn index, block hash in context
    
    distinguish code address by storage slot (#12)
    
    code address for mapping keys (#13)
    
    return gas price of execution
    
    ignore init code size limit (#15)
    
    api change
    
    code address field (#16)
    
    emit output for revert (#6)
    
    rebase upstream
    
    fix stack encoding
    
    fix gas type
    
    fix flags
    
    no reset state in traceCallMany
    
    add tx origin override
    
    mock functions
    
    fix
    
    support storage in sentio tracer
    
    add storage addresses
    
    cleanup

--- core/vm/evm.go
+++ core/vm/evm.go
@@ -27,6 +27,7 @@ import (
 
 	"github.com/erigontech/erigon-lib/chain"
 	libcommon "github.com/erigontech/erigon-lib/common"
+	"github.com/erigontech/erigon-lib/common/hexutility"
 	"github.com/erigontech/erigon-lib/common/u256"
 	"github.com/erigontech/erigon-lib/crypto"
 	"github.com/erigontech/erigon-lib/trie"
@@ -213,12 +214,24 @@ func (evm *EVM) call(typ OpCode, caller ContractRef, addr libcommon.Address, inp
 
 	snapshot := evm.intraBlockState.Snapshot()
 
+	mock := false
+	var mockOutput []byte
+	if len(evm.config.MockFunctions) > 0 && (typ == CALL || typ == CALLCODE) && len(input) >= 4 {
+		if mocks, ok := evm.config.MockFunctions[addr]; ok {
+			sig := hexutility.Encode(input[:4])
+			if output, ok := mocks[sig]; ok {
+				mock = true
+				mockOutput = output
+			}
+		}
+	}
+
 	if typ == CALL {
 		exist, err := evm.intraBlockState.Exist(addr)
 		if err != nil {
 			return nil, 0, fmt.Errorf("%w: %w", ErrIntraBlockStateFailed, err)
 		}
-		if !exist {
+		if !exist && !mock {
 			if !isPrecompile && evm.chainRules.IsSpuriousDragon && value.IsZero() {
 				if evm.config.Debug {
 					v := value
@@ -271,7 +284,9 @@ func (evm *EVM) call(typ OpCode, caller ContractRef, addr libcommon.Address, inp
 	}
 
 	// It is allowed to call precompiles, even via delegatecall
-	if isPrecompile {
+	if mock {
+		ret, err = mockOutput, nil
+	} else if isPrecompile {
 		ret, gas, err = RunPrecompiledContract(p, input, gas)
 	} else if len(code) == 0 {
 		// If the account has no code, we can abort here
@@ -383,6 +398,17 @@ func (evm *EVM) create(caller ContractRef, codeAndHash *codeAndHash, gasRemainin
 	var gasConsumption uint64
 	depth := evm.interpreter.Depth()
 
+	if evm.config.CreateAddressOverride != nil {
+		address = *evm.config.CreateAddressOverride
+	}
+	if evm.config.CreationCodeOverrides != nil {
+		if code, ok := evm.config.CreationCodeOverrides[address]; ok {
+			codeAndHash.code = code
+			codeAndHash.hash = libcommon.Hash{}
+			_ = codeAndHash.Hash()
+		}
+	}
+
 	if evm.config.Debug {
 		if depth == 0 {
 			evm.config.Tracer.CaptureStart(evm, caller.Address(), address, false /* precompile */, true /* create */, codeAndHash.code, gasRemaining, value, nil)
@@ -429,18 +455,20 @@ func (evm *EVM) create(caller ContractRef, codeAndHash *codeAndHash, gasRemainin
 	if evm.chainRules.IsBerlin {
 		evm.intraBlockState.AddAddressToAccessList(address)
 	}
-	// Ensure there's no existing contract already at the designated address
-	contractHash, err := evm.intraBlockState.ResolveCodeHash(address)
-	if err != nil {
-		return nil, libcommon.Address{}, 0, fmt.Errorf("%w: %w", ErrIntraBlockStateFailed, err)
-	}
-	nonce, err := evm.intraBlockState.GetNonce(address)
-	if err != nil {
-		return nil, libcommon.Address{}, 0, fmt.Errorf("%w: %w", ErrIntraBlockStateFailed, err)
-	}
-	if nonce != 0 || (contractHash != (libcommon.Hash{}) && contractHash != trie.EmptyCodeHash) {
-		err = ErrContractAddressCollision
-		return nil, libcommon.Address{}, 0, err
+	if evm.config.CreateAddressOverride == nil {
+		// Ensure there's no existing contract already at the designated address
+		contractHash, err := evm.intraBlockState.ResolveCodeHash(address)
+		if err != nil {
+			return nil, libcommon.Address{}, 0, fmt.Errorf("%w: %w", ErrIntraBlockStateFailed, err)
+		}
+		nonce, err := evm.intraBlockState.GetNonce(address)
+		if err != nil {
+			return nil, libcommon.Address{}, 0, fmt.Errorf("%w: %w", ErrIntraBlockStateFailed, err)
+		}
+		if nonce != 0 || (contractHash != (libcommon.Hash{}) && contractHash != trie.EmptyCodeHash) {
+			err = ErrContractAddressCollision
+			return nil, libcommon.Address{}, 0, err
+		}
 	}
 	// Create a new account on the state
 	snapshot := evm.intraBlockState.Snapshot()
@@ -462,7 +490,7 @@ func (evm *EVM) create(caller ContractRef, codeAndHash *codeAndHash, gasRemainin
 	ret, err = run(evm, contract, nil, false)
 
 	// EIP-170: Contract code size limit
-	if err == nil && evm.chainRules.IsSpuriousDragon && len(ret) > evm.maxCodeSize() {
+	if err == nil && !evm.config.IgnoreCodeSizeLimit && evm.chainRules.IsSpuriousDragon && len(ret) > evm.maxCodeSize() {
 		// Gnosis Chain prior to Shanghai didn't have EIP-170 enabled,
 		// but EIP-3860 (part of Shanghai) requires EIP-170.
 		if !evm.chainRules.IsAura || evm.config.HasEip3860(evm.chainRules) {
@@ -480,6 +508,9 @@ func (evm *EVM) create(caller ContractRef, codeAndHash *codeAndHash, gasRemainin
 	// by the error checking condition below.
 	if err == nil {
 		createDataGas := uint64(len(ret)) * params.CreateDataGas
+		if evm.config.IgnoreGas {
+			createDataGas = 0
+		}
 		if contract.UseGas(createDataGas, tracing.GasChangeCallCodeStorage) {
 			evm.intraBlockState.SetCode(address, ret)
 		} else {
